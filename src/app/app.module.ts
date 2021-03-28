ee108b8d4094c9cff8045d007ef07cc6e0151a99
someyaaltous <someyaaltous1995@gmail.com>
2021-03-28 20:27:57 +0300

    added modules for dark mode theme service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,32 +4,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CountriesComponent } from './pages/countries-component/countries-component.component';
-import {HttpClientModule} from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DetailsCountryComponent } from './pages/details-country/details-country.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
-import {routingTable} from './routes'
+import { routingTable } from './routes';
 import { RouterModule } from '@angular/router';
 
 import { HeaderComponent } from './components/header/header.component';
+import { ThemeService } from './components/header/theme.service';
 
-import {CountryService} from './services/country.service'
+import { CountryService } from './services/country.service';
 @NgModule({
   declarations: [
     AppComponent,
     CountriesComponent,
     DetailsCountryComponent,
     PageNotFoundComponent,
-    HeaderComponent
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
     FormsModule,
-    RouterModule.forRoot(routingTable)
+    ReactiveFormsModule,
+    RouterModule.forRoot(routingTable),
   ],
-  providers: [CountryService],
-  bootstrap: [AppComponent]
+  providers: [CountryService, ThemeService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
